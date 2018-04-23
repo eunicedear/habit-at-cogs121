e1da1bad7864c2fe0bca2a571a2e84561031d255
Kellie <kkhiga@ucsd.edu>
2018-04-23 04:52:20 -0700

    Added data to fake database

--- server.js
+++ server.js
@@ -16,10 +16,18 @@ const fakeUserDatabase = {
 };
 
 const fakeHabitsDatabase = {
-    '1': {title:'Sleep Earlier', description:'Sleep by 8pm every day', due:'09-14-2018', status:'complete'},
+    '4': [
+      {title:'Sleep Earlier Every Day', description:'Sleep by 8pm every day', due:'09-14-2018', status:'complete'},
+      {title:'Eat Vegetables', description:'Eat a serving of vegetables every day', due:'10-10-2019', status:'incomplete'},
+      {title:'Read More', description:'Read 10 pages or more a day', due:'12-12-2020', status:'complete'}
+    ],
     '2': {title:'Eat Vegetables', description:'Eat a serving of vegetables every day', due:'10-10-2019', status:'incomplete'},
-    '4': {title:'Read More', description:'Read 10 pages or more a day', due:'12-12-2020', status:'complete'}
+    '1': {title:'Read More', description:'Read 10 pages or more a day', due:'12-12-2020', status:'complete'}
 };
+
+function isUser(useremail) {
+    return useremail.name === 'cherries';
+}
 
 app.get('/', (req,res) => {
   if(currentUser == null) {
@@ -32,6 +40,7 @@ app.get('/', (req,res) => {
 
 app.get('/habits', (req,res) => {
   const val = fakeHabitsDatabase[currentUser];
+  console.log('Current User:', currentUser);
   console.log(currentUser, '->', val);
   if(val) {
     res.send(val);
@@ -42,10 +51,10 @@ app.get('/habits', (req,res) => {
 
 app.get('/login/:userid', (req,res) => {
   const userid = req.params.userid;
-  const val = fakeUserDatabase[userid];
   console.log(userid, '->', val.userid);
   if(val) {
     currentUser = val.userid;
+
     res.send(val.userid);
   } else {
     res.send({});
