3d7f600e029bc3b754c17e5b829dccaf1a33b161
Kellie <kkhiga@ucsd.edu>
2018-05-02 09:33:37 -0700

    Home/Pet Page front-end

---
+++ static/home.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+      if (pair[0] == variable) {
+        return pair[1];
+      }
+    }
+    return (false);
+  }
+  console.log(window.location.search);
+  const childid = getQueryVariable("childid");
+  console.log(childid);
+  const requestURL = "/home/" + childid;
+  console.log(requestURL);
+
+  $.ajax({
+    url: requestURL,
+    type: 'GET',
+    dataType: 'json', // this URL returns data in JSON format
+    success: (data) => {
+      console.log('You received some data!', data);
+      if (data.name && data.pet) {
+        $('#points').html(data.points + " Pts");
+      } else {
+          console.log("Child info not received");
+      }
+    },
+  });
+});
