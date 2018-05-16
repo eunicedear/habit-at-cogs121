d73675ca78f337d01246c365709b4cf1fdf84aaf
Kellie <kkhiga@ucsd.edu>
2018-05-15 17:20:59 -0700

    Refactoring server code

--- server.js
+++ server.js
@@ -3,24 +3,45 @@ const app = express();
 
 app.use(express.static('static'));
 
-console.log('Your Directory Name: ' + __dirname);
-
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('habits.db');
+const bodyParser = require('body-parser');
 
-// Track login status
-var currentUser = null;
-var currentChild = null;
-
-
+// Navigate to 'localhost:3000/'
 app.get('/', (req, res) => {
-  if (currentUser == null) {
-    res.redirect('login.html');
-  } else {
-    console.log('User' + currentUser + ' already logged in')
     res.redirect('accounts.html');
-  }
 });
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  db.all('SELECT * from users_to_accounts WHERE username=$username', {
+    $username: req.body.username
+}, (err, data) => {
+    //
+    if (data) {
+      console.log('User Found:', data[0]);
+      // Check if password sent matches password in database
+      if (req.body.password == data[0].password) {
+        console.log('Correct Password Entered, Logging in User');
+        console.log('Sending Data', data[0]);
+        // Sends the result of the query
+        res.send(data[0]);
+      } else {
+        console.log('Incorrect Password Entered!');
+        // Sends an error status
+        res.sendStatus(401);
+      }
+    } else {
+      console.log('User Not Found');
+      // Sends an error status
+      res.sendStatus(401);
+    }
+  });
+});
+
 
 app.get('/habits', (req, res) => {
   db.all(
@@ -37,68 +58,76 @@ app.get('/habits', (req, res) => {
   );
 });
 
-app.get('/accounts', (req, res) => {
-  db.all(
-    'SELECT * FROM children_to_user WHERE userid=$userid', {
-      $userid: currentUser
-    },
-    (err, rows) => {
-      if(rows.length > 0) {
-        res.send(rows);
-      } else {
-        res.send({});
-      }
-    }
-  );
-});
-
-app.get('/home/:childid', (req, res) => {
-  const childToLookup = req.params.childid;
-  db.all(
-    'SELECT * FROM children_to_user WHERE childid=$childid',
-    {
-      $childid: childToLookup
-    },
-    (err, rows) => {
-      console.log(rows);
-      if(rows.length > 0) {
-        res.send(rows[0]);
-        currentChild = rows[0].chlidid;
-      } else {
-        res.send({});
-      }
-    }
-  );
-});
-
-//POST request is for logging inspect to the Server
-const bodyParser = require('body-parser');
+// Request for user's child data
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.post('/login', (req, res) => {
-  console.log(req.body);
-  db.all('SELECT * from users_to_accounts WHERE username=$username', {
-    $username: req.body.username
-  }, (err, rows) => {
-    if (rows[0]) {
-      console.log('User Found:', rows[0]);
-      if (req.body.password == rows[0].password) {
-        console.log('Correct Password Entered!');
-        currentUser = rows[0].userid;
-        res.send({
-          message: 'Success!'
-        });
-      } else {
-        console.log('Incorrect Password Entered!');
-        res.sendStatus(401);
-      }
+app.post('/accounts', (req, res) => {
+    console.log('Request at /accounts body: ', req.body);
+    if(req.body.userid == null) {
+        // Redirect to Login if no userid
+        res.redirect('login.html');
     } else {
-      console.log('User Not Found');
-      res.sendStatus(401);
+        // Query user's children accounts and send data (if any)
+        db.all(
+          'SELECT * FROM children_to_user WHERE userid=$userid', {
+            $userid: req.body.userid
+          },
+          (err, rows) => {
+            if(rows.length > 0) {
+              res.send(rows);
+            } else {
+              res.send({});
+            }
+          }
+        );
     }
-  });
 });
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.post('/home', (req, res) => {
+  db.all(
+    'SELECT * FROM children_to_user WHERE childid=$childid',
+    {
+      $childid: req.body.childid
+    },
+    (err, data) => {
+      if(data.length > 0) {
+          console.log('Sending data about child', data[0]);
+          res.send(data[0]);
+          // res.redirect('home.html');
+      } else {
+          console.log('No data, sending empty {}');
+        res.send({});
+      }
+    }
+  );
+});
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.post('/verifyChild', (req, res) => {
+  db.all(
+    'SELECT * FROM children_to_user WHERE childid=$childid',
+    {
+      $childid: req.body.childid
+    },
+    (err, data) => {
+      if(data.length > 0) {
+          console.log('Redirecting to homepage or child', data[0]);
+          res.redirect('home.html');
+      } else {
+          console.log('Child does not exist, sending empty {}');
+        res.send({});
+      }
+    }
+  );
+});
+
+
 
 app.post('/createAccount', (req, res) => {
   console.log(req.body);
@@ -148,7 +177,7 @@ app.post('/createChild', (req, res) => {
   console.log('body', req.body);
   db.run(
     'INSERT INTO children_to_user (userid, name, pet, points) VALUES ($userid, $name, $pet, 0)', {
-      $userid: currentUser,
+      $userid: req.body.userid,
       $name: req.body.name,
       $pet: req.body.pet
     },
