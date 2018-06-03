a4de46fe9133c7f33494411f1079a803af9993e8
Kellie <kkhiga@ucsd.edu>
2018-06-03 00:08:15 -0700

    Add firebase to closet.js

--- static/closet.js
+++ static/closet.js
@@ -1,3 +1,16 @@
+var userId, database, childId, habitId;
+var config = {
+  apiKey: "AIzaSyANhXaRoHHK8S06Y54SU_lqwmzDMBpiGfI",
+  authDomain: "tycho-habit-at.firebaseapp.com",
+  databaseURL: "https://tycho-habit-at.firebaseio.com",
+  projectId: "tycho-habit-at",
+  storageBucket: "tycho-habit-at.appspot.com",
+  messagingSenderId: "879811116990"
+};
+
+firebase.initializeApp(config);
+console.log("Initializing Firebase");
+
 function handleItemClick(element) {
   let accessoryid = element.getAttribute("accessoryid");
   console.log('accessory ', accessoryid, ' clicked');
@@ -11,38 +24,35 @@ function handleItemClick(element) {
 
 // Matches with items with attribute -- onclick="handleItemClick(this)"
 function updatePetImage(accessoryid) {
-  // get url from child-to-accessories table using id from localStorage? Can u push your code
-  $.ajax({
-    url: 'pet-preview',
-    type: 'POST',
-    data: {
-      accessoryid: accessoryid
-    },
-    success: (data) => {
-      console.log('Data received, url: ', data);
-      $('#pet-img').attr('src', "assets/accessories_on/" + data[0].dogUrl);
-    }
-  });
+  // $.ajax({
+  //   url: 'pet-preview',
+  //   type: 'POST',
+  //   data: {
+  //     accessoryid: accessoryid
+  //   },
+  //   success: (data) => {
+  //     console.log('Data received, url: ', data);
+  //     $('#pet-img').attr('src', "assets/accessories_on/" + data[0].dogUrl);
+  //   }
+  // });
 }
 
-// LOL im dum
-// $('.items').click(() => {
-//   console.log("clicked");
-//   $(this).toggleClass("selected_btn");
-// });
-
-// User clicks on an accessory
-
-// $(document).on('click', '.items', function() {
-//   // toggles selection when clicked, makes selected item grey
-//   $(this).toggleClass("selected_btn");
-//
-//   // Store in localStorage the id of the accessory clicked
-//   $('.items').click( () => {
-//     let accessoryid = $(this).getAttribute('accessoryid'); // getattribute not a fucntion? wait we dont need this anymoreO at all?
-//     // store id in localStorage
-//     localStorage.setItem('accessoryid', accessoryid);
-//
-//
-//   });
-// });
+function initApp() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      userId = user.uid;
+      database = firebase.database();
+      childId = localStorage.getItem("childid");
+      console.log("User is signed in.", userId);
+      console.log("Current child: ", childId)
+      viewHome();
+    } else {
+      // User is signed out.
+      console.log("User is signed out.");
+      location.href = "login.html";
+    }
+  }, function(error) {
+    console.log(error);
+  });
+};
